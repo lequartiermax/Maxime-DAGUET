f39e538b7a939e5668398bbce9a0cda2ce6dec9d
lequartiermax <lequartier.max@gmail.com>
2026-05-21 14:15:54 +0000

    continuité tentative générer graph

--- app.py
+++ app.py
@@ -1,22 +1,16 @@
 import plotly.express as px
 import pandas as pd
 
-données = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSC4KusfFzvOsr8WJRgozzsCxrELW4G4PopUkiDbvrrV2lg0S19-zeryp02MC9WYSVBuzGCUtn8ucZW/pub?output=csv')
+données = pd.read_csv('http://github.com/lequartiermax/Maxime-DAGUET/blob/main/ventes%20-%20ventes.csv')
 
-figure = px.pie(données, values='qte', names='région', title='quantité vendue par région')
+figure = px.pie(données, values='qte', names='region', title='quantité vendue par region')
 
-figure.write_html('ventes-par-région.html')
-
-print('ventes-par-région.html généré avec succès !')
+figure.write_html('ventes-par-region.html')
 
 figure = px.pie(données, values='qte', names='produit', title='quantité vendue par produit')
 
-figure.write_html('CA-par-produit.html')
+figure.write_html('ventes-par-région.html')
 
-print('CA-par-produit.html généré avec succès !')
+figure = px.pie(données, values='CA', names='produit', title='CA par produit' )
 
-figure = px.pie(données, values='prix', names='produit', title='CA par produit')
-
-figure.write_html('CA-par-produit.html')
-
-print('CA-par-produit.html généré avec succès !')
+figure.write_html('CA-par-région.html')
