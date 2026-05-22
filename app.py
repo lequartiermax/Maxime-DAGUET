16bb432bd2a61cd409ad2ef2449a69404e131381
lequartiermax <lequartier.max@gmail.com>
2026-05-22 09:44:17 +0000

    soltuion trouvée, production graphique générée

--- app.py
+++ app.py
@@ -3,21 +3,20 @@ import pandas as pd
 
 données = pd.read_csv('https://raw.githubusercontent.com/lequartiermax/Maxime-DAGUET/main/ventes%20-%20ventes.csv')
 
-figure = px.pie(données, values='qte', names='region', title='quantité vendue par region')
-
-figure.write_html('ventes-par-region.html')
-
-figure.show()
-
-figure = px.pie(données, values='qte', names='produit', title='quantité vendue par produit')
+figure = px.pie(données, values='qte', names='region', title='quantité vendue par région')
 
 figure.write_html('ventes-par-région.html')
 
-figure.show()
+print('ventes-par-région')
+
+figure = px.pie(données, values='qte', names='produit', title='quantité vendue par produit')
+
+figure.write_html('ventes-par-produit.html')
+
+print('ventes-par-produit')
 
 figure = px.pie(données, values='CA', names='produit', title='CA par produit' )
 
 figure.write_html('CA-par-produit.html')
 
-figure.show()
-
+print('CA-par-produit')
